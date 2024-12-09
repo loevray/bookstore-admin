bd5cfabcc5c7fbeada31d8f092161492ea9d4159
loevray <rei980213@gmail.com>
2024-12-10 00:20:28 +0900

    Test: add get books api test

---
+++ src/__tests__/api/books.test.ts
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment node
+ */
+
+import {GET} from '@/app/api/books/route'
+import BOOKS from '@/mocks/books';
+
+describe('/api/books API test', () => {
+  test('return 200 status and correct data', async () => {
+    
+    const response = await GET();
+
+    expect(response.status).toBe(200);
+    
+    const data = await response.json();
+    
+    expect(data).toEqual(BOOKS);;
+  });
+
+});
