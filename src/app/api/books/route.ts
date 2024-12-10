df02bb9459fa9b1072da09400fa5cbb80fbd59a1
loevray <rei980213@gmail.com>
2024-12-10 12:24:07 +0900

    Refactor: get books api connect to db

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,9 +1,21 @@
-import BOOKS from '@/mocks/books';
-import {NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
+import db from '@/app/firebase/firebasedb'
+import { collection, getDocs } from 'firebase/firestore';
+import booksConverter from '@/app/firebase/booksConverter';
 
-
-export async function GET() {
-  return NextResponse.json(BOOKS);
+export interface I_Books {
+  id:number,
+  title:string,
+  author:string,
+  plot:string,
+  publicationYear:number,
+  publisher:string,
+  price:number,
 }
 
 
+export async function GET() {
+  const books = await getDocs(collection(db, 'books').withConverter(booksConverter))
+  const booksData = books.docs.map(doc => ({ ...doc.data() }));
+  return NextResponse.json(booksData);
+}
