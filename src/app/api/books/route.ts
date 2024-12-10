7a80f81a5dc6c091ac91998c4c86c0245befc993
loevray <rei980213@gmail.com>
2024-12-10 12:35:00 +0900

    Refactor: get books api excption handling

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import db from '@/app/firebase/firebasedb'
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, FirestoreError, getDocs } from 'firebase/firestore';
 import booksConverter from '@/app/firebase/booksConverter';
 
 export interface I_Books {
@@ -15,7 +15,25 @@ export interface I_Books {
 
 
 export async function GET() {
-  const books = await getDocs(collection(db, 'books').withConverter(booksConverter))
-  const booksData = books.docs.map(doc => ({ ...doc.data() }));
-  return NextResponse.json(booksData);
+  try{
+    const books = await getDocs(collection(db, 'books').withConverter(booksConverter))
+    const booksData = books.docs.map(doc => ({ ...doc.data() }));
+    return NextResponse.json(booksData);
+  } catch(error){
+    if(error instanceof FirestoreError){
+      //FirestoreError에서 제공하는 code별 분류 필요
+      return NextResponse.json({ message: error.code }, { status: 400});
+    }
+    
+    let errorMessage;
+    if(error instanceof Error) {
+      errorMessage = error.message;
+    } else {
+      errorMessage = String(error);
+    }
+    
+    console.error('Error fetching books:', errorMessage); 
+    return NextResponse.json({ message: errorMessage }, { status: 500});
+  }
+
 }
