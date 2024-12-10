ab50f5575ccf518e577237627ce930d9d625aa0f
loevray <rei980213@gmail.com>
2024-12-10 21:36:26 +0900

    Remove: useless code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import BookList from './_components/book/BookList';
-import { fetchBookList } from './lib/services/api';
 import ApiQueryWrapper from './_components/book/ApiQueryLayer';
 import BookListWrapper from './_components/book/BookListWrapper';
 
 export default async function Home() {
-  const initialBookData = await fetchBookList();
-
   return (
     <div>
       <main>
