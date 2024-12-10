260e7ac3b177c1ed2ae0456b05b1748f0095c414
loevray <rei980213@gmail.com>
2024-12-10 20:19:32 +0900

    Style: change api client name getBookList => fetchBookList

--- src/app/lib/services/api.tsx
+++ src/app/lib/services/api.tsx
@@ -1,6 +1,6 @@
 import { I_Books } from '@/app/api/books/route';
 
-export default async function getBookList(): Promise<I_Books[]> {
+export async function fetchBookList(): Promise<I_Books[]> {
   try {
     const response = await fetch('http://localhost:3000/api/books');
     const data = await response.json();
