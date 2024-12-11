5a0cd0411bc93109a2ee5a6e8246c16d4342def6
loevray <rei980213@gmail.com>
2024-12-11 10:51:47 +0900

    Style: rename ApiQueryLayer => ApiQueryWrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import ApiQueryWrapper from './_components/book/ApiQueryLayer';
+import ApiQueryWrapper from './_components/book/ApiQueryWrapper';
 import BookListWrapper from './_components/book/BookListWrapper';
 import BookSearchForm from './_components/book/BookSearchForm';
 
