b8624db69459928fb0505d59320e54d117185de8
loevray <rei980213@gmail.com>
2024-12-11 12:35:37 +0900

    Feat: add fetchBook api client

--- src/app/lib/services/api.tsx
+++ src/app/lib/services/api.tsx
@@ -22,3 +22,18 @@ export async function fetchBookList({
     throw e;
   }
 }
+
+export async function fetchBook({ id }: { id: string }): Promise<I_Books> {
+  try {
+    if (!id) throw new Error('책 id가 존재하지 않습니다');
+    const response = await fetch(`http://localhost:3000/api/books/${id}`, {
+      cache: 'no-store',
+      next: { revalidate: 0 },
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    throw e;
+  }
+}
