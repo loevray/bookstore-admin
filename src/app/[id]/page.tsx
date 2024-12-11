25ab2d7ecc7b31307a8a307d8cd29272ff9ab7e6
loevray <rei980213@gmail.com>
2024-12-11 12:41:59 +0900

    Design: styling book detail page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,11 +1,20 @@
+import Link from 'next/link';
 import ApiQueryWrapper from '../_components/book/ApiQueryWrapper';
 import BookDetail from './BookDetail';
 
 export default function BookDetailPage() {
   return (
-    <main>
+    <main className="bg-gray-100 min-h-screen py-8">
       <ApiQueryWrapper>
-        <BookDetail />
+        <section className="container mx-auto px-4">
+          <nav className="mb-4">
+            <Link href="/" className="text-blue-500 hover:underline">
+              Home
+            </Link>
+            / <span>Book Details</span>
+          </nav>
+          <BookDetail />
+        </section>
       </ApiQueryWrapper>
     </main>
   );
